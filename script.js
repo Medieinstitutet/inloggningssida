c3ecd2daa49df7288aa002a46657c67a53651b95
Eraze86 <therese.rosvall@hotmail.se>
2021-12-03 13:23:03 +0100

    går att logga in, fel användare. WIP

--- script.js
+++ script.js
@@ -2,22 +2,66 @@
 const header = document.getElementById("header")
 const welcome = document.getElementById("welcome")
 const loggIn = document.getElementById("btn")
-const username = document.getElementById("use")
-const password = document.getElementById("password")
-//skapa en function 
-function changeWelcome (){
-welcome.innerHTML = "<h1>välkommen</h1>" + username; 
-username.innerHTML = "användarnamn";
-password.innerHTML = "Janne";
-loggIn.InnerHTML = "<button>Logg out<button>";
+const username = document.getElementById("username").value
+const password = document.getElementById("password").value
+const wrongUP = document.getElementById("wrongup");
+
+//min array
+let user = [
+    {
+        username: "janne",
+        password: "test"
+    },
+    {
+        username: "anna",
+        password: "panna"
+    },
+    {
+        username: "petter",
+        password: "getter"
+    }
+]
+localStorage.setItem("users", user)
+//när man trycker på knappen startar funktionen
+loggIn.addEventListener("click", () => {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+for (let i = 0; i < user.length; i++) {
+//om användarnman och lösenord stämmer. function change startar
+    if (username == user[i].username && password == user[i].password) {
+    
+        change()
+    }
+    //om det inte stämmer med databasen, så skickas man till functionen wrong
+    else{
+        wrong()
+    } 
+}
+})
+
+//när man är inloggad visas ens användarnamn och en logga ut knapp
+function change() {
+    welcome.innerHTML = "<h1>välkommen</h1>" + username;
+    header.innerHTML = `Användarnamn: ` + user.username + `<button id="loggOut">Logga ut</button>`;
 };
 
-loggIn.addEventListener("click", () => {
-changeWelcome()
+//fel user. eller pass. komemr en ruta upp med text och en stäng knapp
+function wrong() {
+wrongUP.innerHTML = "Fel användarnamn eller lösenord, försök igen!";
+let closewin = document.createElement("button");
+closewin.textContent = "stäng";
+wrongUP.append(closewin)
+//trycker man på stäng knappen så stängs rutan. innerHTML gör att den töms men försvinner inte ur dom
+closewin.addEventListener("click", () =>{
+    wrongUP.innerHTML = "";
+   
+})
+}
 
-console.log("hej")
 
-});
+
+
+
 //en knapp, logga in, som startar functionen när man loggar in
 
 ///en knapp, logga ut, som upphäver functionen när man loggar ut
