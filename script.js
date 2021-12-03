e2f77b579ddb231435f24c2cd1e1eb9497ed0fec
Eraze86 <therese.rosvall@hotmail.se>
2021-12-03 14:28:58 +0100

    logga ut fungerar, hämtar användare till localstorage, fortsätta med localStorgae

--- script.js
+++ script.js
@@ -5,58 +5,63 @@ const loggIn = document.getElementById("btn")
 const username = document.getElementById("username").value
 const password = document.getElementById("password").value
 const wrongUP = document.getElementById("wrongup");
+let loggOut = document.createElement("button");
 
 //min array
 let user = [
     {
         username: "janne",
         password: "test"
-    },
-    {
-        username: "anna",
-        password: "panna"
-    },
-    {
-        username: "petter",
-        password: "getter"
     }
 ]
-localStorage.setItem("users", user)
+
+
+
 //när man trycker på knappen startar funktionen
 loggIn.addEventListener("click", () => {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
-for (let i = 0; i < user.length; i++) {
-//om användarnman och lösenord stämmer. function change startar
-    if (username == user[i].username && password == user[i].password) {
-    
-        change()
+    for (let i = 0; i < user.length; i++) {
+        //om användarnman och lösenord stämmer. function change startar
+        if (username == user[i].username && password == user[i].password) {
+            change()
+        }
+        //om det inte stämmer med databasen, så skickas man till functionen wrong
+        else if (username !== user[i].username && password !== user[i].password) {
+            wrong()
+        }
     }
-    //om det inte stämmer med databasen, så skickas man till functionen wrong
-    else{
-        wrong()
-    } 
-}
 })
 
 //när man är inloggad visas ens användarnamn och en logga ut knapp
 function change() {
+    if (localStorage.getItem("users") === null) {
+        localStorage.setItem("users", JSON.stringify(user));
+    }
     welcome.innerHTML = "<h1>välkommen</h1>" + username;
-    header.innerHTML = `Användarnamn: ` + user.username + `<button id="loggOut">Logga ut</button>`;
-};
+    header.innerHTML = `Användarnamn: ` + user.username;
+    loggOut.textContent = "logga ut"
+    header.append(loggOut)
 
+    loggOut.addEventListener("click", () => {
+
+        location.reload()
+    })
+
+};
 //fel user. eller pass. komemr en ruta upp med text och en stäng knapp
 function wrong() {
-wrongUP.innerHTML = "Fel användarnamn eller lösenord, försök igen!";
-let closewin = document.createElement("button");
-closewin.textContent = "stäng";
-wrongUP.append(closewin)
-//trycker man på stäng knappen så stängs rutan. innerHTML gör att den töms men försvinner inte ur dom
-closewin.addEventListener("click", () =>{
-    wrongUP.innerHTML = "";
-   
-})
+    wrongUP.innerHTML = "Fel användarnamn eller lösenord, försök igen!";
+    let closewin = document.createElement("button");
+    closewin.textContent = "stäng";
+    wrongUP.append(closewin)
+    //trycker man på stäng knappen så stängs rutan. innerHTML gör att den töms men försvinner inte ur dom
+    closewin.addEventListener("click", () => {
+        wrongUP.innerHTML = "";
+
+    })
 }
+//när man loggar ut ska man komma tillbaka till start sidan
 
 
 
